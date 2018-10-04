31385f78b37b57a0575967a7e1769424844a4797
Javier E. Perez P <javier.perez@entelgy.com>
2018-10-04 16:09:06 -0500

    fin de clase11, controlador articles

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'bienvenido/index'
 
-  resources :articulos
+  resources :articles
 
   root 'bienvenido#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
